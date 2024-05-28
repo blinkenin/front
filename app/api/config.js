74eb3a6b9a91c337ad74f964b0f0bf3745043d3d
blinkenin <frost700iq@gmail.com>
2024-05-28 17:07:17 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://frontex.nomoredomainswork.ru/'
+export const BASE_URL = 'https://frontex.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
